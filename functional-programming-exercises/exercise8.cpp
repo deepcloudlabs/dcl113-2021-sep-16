9cbf63a5557e91327c2bc5ef57aa3d83fd48f896
deepcloudlabs <deepcloudlabs@gmail.com>
2021-09-17 19:03:30 +0300

    initial commit

--- functional-programming-exercises/exercise8.cpp
+++ functional-programming-exercises/exercise8.cpp
@@ -3,14 +3,16 @@
 #include "world-util.h"
 
 #include <map>
+#include <vector>
 #include <iostream>
 #include <string>
 #include <memory>
-#include <algorithm>
 #include <numeric>
 
 using namespace std;
 using namespace world;
+
+// TODO: Find the capital with highest population in the world
 
 map<int,shared_ptr<city>> cities;
 map<string,shared_ptr<country>> countries;
@@ -18,7 +20,21 @@ map<string,shared_ptr<country>> countries;
 int main(int argc, char* argv[]){
     create_world();
 
-    // TODO: Find the capital with highest population in the world
+    auto highPopulatedCapitalReducer=  [](shared_ptr<city>& highPopulatedCapital, const pair<const string,shared_ptr<country>>& entry)  {
+        auto capital= entry.second->capital;
+        if(capital==nullptr) return highPopulatedCapital;
+        auto population= capital->population;
+        if (highPopulatedCapital==nullptr){
+            return capital;
+        } else  if (highPopulatedCapital->population<population) {
+            return capital;
+        }
+        return highPopulatedCapital;
+    };
+
+    auto highPopulatedCapital = accumulate(countries.cbegin(),countries.cend(),shared_ptr<city>(nullptr),highPopulatedCapitalReducer);
+
+    cout << *highPopulatedCapital << endl;
 
     return 0;
 }
